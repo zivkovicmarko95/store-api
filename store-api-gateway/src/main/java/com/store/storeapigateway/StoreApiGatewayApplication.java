a3fd9f0079e4ec720c391a3100aa23d7748b7dc0
zivkovic.marko <zivkovic.marko.ftn@gmail.com>
2021-10-09 16:56:08 +0200

    Add API gateway component

---
+++ store-api-gateway/src/main/java/com/store/storeapigateway/StoreApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.store.storeapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreApiGatewayApplication.class, args);
+	}
+
+}
---
+++ store-api-gateway/src/main/java/com/store/storeapigateway/configs/SecurityConfig.java
@@ -0,0 +1,47 @@
+package com.store.storeapigateway.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoders;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+@EnableReactiveMethodSecurity
+public class SecurityConfig {
+    
+    private final String jwkSetUri = "http://localhost:8080/auth/realms/store-api";
+
+    private final String[] PUBLIC_URLS = {
+        "/api/auth/login",
+        "/api/products",
+        "/api/categories"
+    };
+
+    @Bean
+    SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        
+        http.authorizeExchange()
+            .pathMatchers(PUBLIC_URLS)
+            .permitAll()
+            .anyExchange()
+            .authenticated().and().oauth2ResourceServer().jwt();
+
+        http.csrf().disable();
+
+        return http.build();
+    }
+
+    @Bean
+    public ReactiveJwtDecoder jwtDecoder() {
+        NimbusReactiveJwtDecoder jwtDecoder = (NimbusReactiveJwtDecoder) ReactiveJwtDecoders.fromIssuerLocation(jwkSetUri);
+        
+        return jwtDecoder;
+    }
+
+}
