25c8ccf7a7db47642ecf60d9d6e9bf26a7d6e744
zivkovic.marko <zivkovic.marko.ftn@gmail.com>
2022-01-28 20:50:08 +0100

    Add securit config

---
+++ store-management-api/src/main/java/com/store/storemanagementapi/configurations/SecurityConfig.java
@@ -0,0 +1,54 @@
+package com.store.storemanagementapi.configurations;
+
+import com.store.storemanagementapi.utils.KeycloakRoleConverter;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    
+    private final String jwkSetUri = "http://localhost:8080/auth/realms/store-api/protocol/openid-connect/certs";
+    private final String[] permitUrls = {  "/api/stores/**" };
+    private final String internalEndpoints = "/api/internal/**";
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests(authorize -> {
+            try {
+                authorize.antMatchers(permitUrls).permitAll()
+                    .antMatchers(internalEndpoints).hasRole("admin")
+                    .anyRequest().authenticated()
+                    .and()
+                    .oauth2ResourceServer()
+                    .jwt()
+                    .jwtAuthenticationConverter(jwtAuthenticationConverter());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    // Necessary to decode JWT token to get roles
+    private Converter<Jwt, ? extends AbstractAuthenticationToken> jwtAuthenticationConverter() {
+        JwtAuthenticationConverter jwtConverter = new JwtAuthenticationConverter();
+        jwtConverter.setJwtGrantedAuthoritiesConverter(new KeycloakRoleConverter());
+        return jwtConverter;
+    }
+
+    @Bean
+    JwtDecoder jwtDecoder() {
+        return NimbusJwtDecoder.withJwkSetUri(this.jwkSetUri).build();
+    }
+
+}
---
+++ store-management-api/src/test/java/com/store/storemanagementapi/its/internal/InternalStoreIT.java
@@ -0,0 +1,5 @@
+package com.store.storemanagementapi.its.internal;
+
+class InternalStoreIT {
+    
+}
