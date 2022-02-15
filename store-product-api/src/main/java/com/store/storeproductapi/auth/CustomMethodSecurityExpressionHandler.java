58c7cd067494f1cc18ea897373de919d9b872727
zivkovic.marko <zivkovic.marko.ftn@gmail.com>
2022-02-15 17:45:58 +0100

    Minor fix

--- store-product-api/src/main/java/com/store/storeproductapi/auth/CustomMethodSecurityExpressionHandler.java
+++ store-product-api/src/main/java/com/store/storeproductapi/auth/CustomMethodSecurityExpressionHandler.java
@@ -3,10 +3,16 @@ package com.store.storeproductapi.auth;
 import org.aopalliance.intercept.MethodInvocation;
 import org.springframework.security.access.expression.method.DefaultMethodSecurityExpressionHandler;
 import org.springframework.security.access.expression.method.MethodSecurityExpressionOperations;
+import org.springframework.security.authentication.AuthenticationTrustResolver;
+import org.springframework.security.authentication.AuthenticationTrustResolverImpl;
 import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
 
+@Component
 public class CustomMethodSecurityExpressionHandler extends DefaultMethodSecurityExpressionHandler {
     
+    private AuthenticationTrustResolver trustResolver = new AuthenticationTrustResolverImpl();
+
     @Override
     protected MethodSecurityExpressionOperations createSecurityExpressionRoot(Authentication authentication,
             MethodInvocation invocation) {
@@ -14,7 +20,7 @@ public class CustomMethodSecurityExpressionHandler extends DefaultMethodSecurity
         final StoreCustomExpression root = new StoreCustomExpression(authentication);
 
         root.setPermissionEvaluator(getPermissionEvaluator());
-        root.setTrustResolver(getTrustResolver());
+        root.setTrustResolver(trustResolver);
         root.setRoleHierarchy(getRoleHierarchy());
         root.setDefaultRolePrefix(getDefaultRolePrefix());
 
--- store-product-api/src/main/java/com/store/storeproductapi/configurations/CustomSecurityExpressionsAuthorizationConfig.java
+++ store-product-api/src/main/java/com/store/storeproductapi/configurations/CustomSecurityExpressionsAuthorizationConfig.java
@@ -9,11 +9,9 @@ import org.springframework.security.access.expression.DenyAllPermissionEvaluator
 import org.springframework.security.access.expression.method.MethodSecurityExpressionHandler;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @Configuration
-@EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class CustomSecurityExpressionsAuthorizationConfig extends GlobalMethodSecurityConfiguration {
     
     private final ApplicationContext applicationContext;
